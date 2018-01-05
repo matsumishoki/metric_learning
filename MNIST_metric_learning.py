4137973e676bce0ec785d4f1bfc1d59d7db79cb3
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2018-01-05 20:07:49 +0900

    訓練データセットと検証用データセットのlossを出力し，プロットする

--- MNIST_metric_learning.py
+++ MNIST_metric_learning.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 
     # 超パラメータの定義
     learning_rate = 0.0001  # learning_rate(学習率)を定義する
-    max_iteration = 1      # 学習させる回数
+    max_iteration = 100      # 学習させる回数
     batch_size = 200       # ミニバッチ1つあたりのサンプル数
  
     model = M.ConvNet().to_gpu()
@@ -84,6 +84,10 @@ if __name__ == '__main__':
         for batch_indexes in np.split(perm, num_batches):
             x_batch = cuda.to_gpu(X_train[batch_indexes])
             t_batch = cuda.to_gpu(T_train[batch_indexes])
+            
+            # 勾配を初期化する            
+            model.zerograds()
+            
             # 順伝播させる
             y_batch = model.loss_and_accuracy(x_batch, t_batch, True)
             # contrastive lossに入力するy1,y2,tを取得する
@@ -91,41 +95,37 @@ if __name__ == '__main__':
             t1, t2 = F.split_axis(t_batch,2, axis=0)
             t = (t1.array==t2.array).astype(np.int32)        
             loss = F.contrastive(y1, y2, t)
-            print("loss", loss)
-        # 入力画像の一枚目と二枚目を無作為に選ぶ
-#        perm_1 = np.random.permutation(num_train)
-#        perm_2 = np.random.permutation(num_train)
-##        print("perm_1",perm_1)
-##        print("perm_2",perm_2)
-#        # mini batchi SGDで重みを更新させるループ
-#        for batch_indexes in np.array_split(perm_1, num_batches):
-#            x_batch_1 = cuda.to_gpu(X_train[batch_indexes])
-#            t_batch_1 = cuda.to_gpu(T_train[batch_indexes])
-#        
-#        for batch_indexes in np.array_split(perm_2, num_batches):
-#            x_batch_2 = cuda.to_gpu(X_train[batch_indexes])
-#            t_batch_2 = cuda.to_gpu(T_train[batch_indexes])
-#                        
-#            # 勾配を初期化する            
-#            model.zerograds()
-#            # contrastive loss関数に入力するy_1,y_2を取得する
-#            y_1 = model.loss_and_accuracy(x_batch_1, t_batch_1, True)
-#            y_2 = model.loss_and_accuracy(x_batch_2, t_batch_2, True)
-#            # tが同じならば1，異なるならば0を入力する
-#            t=[]
-#            for element in range(batch_size):
-#                if t_batch_1[element] == t_batch_2[element]:
-#                    t.append(1)
-#                else:
-#                    t.append(0)
-#            t = np.array(t)
-#            t =chainer.cuda.to_gpu(t)
-#            F.contrastive(y_1,y_2,t)
-#            print("contrastive",F.contrastive(y_1,y_2,t))
-#            print("t",t)
-#            print("x_batch_1:", x_batch_1)
-#            print("t_batch_1:", t_batch_1)
-#            print("x_batch_2:", x_batch_2)
-#            print("t_batch_2:", t_batch_2)
-
+#            print("loss:", loss)
+            # 逆伝播
+            loss.backward()
+            optimizer.update()
+        time_finish = time.time()
+        time_elapsed = time_finish - time_start
+        print ("time_elapsed:", time_elapsed)
+                       
+        # 訓練データセットの交差エントロピー誤差を表示する
+        train_loss = M.metric_loss_average(
+                model, X_train, T_train, num_batches, False)
+        loss_train_history.append(train_loss)
+        print ("[train] Loss:", train_loss)
+        
+        # 検証用データセットの交差エントロピー誤差を表示する
+#        valid_loss = M.metric_loss_average(
+#                model, X_valid, T_valid, num_batches, False)
+#        loss_valid_history.append(valid_loss)
+#        print ("[valid] Loss:", valid_loss)
+        
+        # 学習曲線をプロットする
+        # plot learning curves
+        plt.subplot(1, 2, 1)
+        plt.title("Loss")
+        plt.plot(loss_train_history)
+        plt.plot(loss_valid_history)
+        plt.legend(["train", "valid"], loc="best")
+        plt.ylim([0.0, 0.05])
+        plt.grid()
+        
+        plt.tight_layout()
+        plt.show()
+        plt.draw()
 
