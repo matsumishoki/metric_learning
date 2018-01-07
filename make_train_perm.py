d246f995fea6ac5d0d98e66fe0571d7773a65018
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2018-01-07 23:02:04 +0900

    単体テストのコードをメイン文に追加する

--- make_train_perm.py
+++ make_train_perm.py
@@ -18,3 +18,10 @@ def make_train_perm_data(T_train, data_size):
         random_indexes_c = np.random.choice(indexes_c, data_size, False)
         indexes.append(random_indexes_c)
     return np.concatenate(indexes)
+
+if __name__ == '__main__':
+    labels = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2])
+    for i in range(10):
+        indexes = make_train_perm_data(labels, 3)
+        np.testing.assert_array_equal(labels[indexes],
+                                      [0, 0, 0, 1, 1, 1, 2, 2, 2])
