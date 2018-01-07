5a675d1cc5fb696f612452fa7a7e23979d6d5d28
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2018-01-08 02:43:34 +0900

    評価するファイルを追加する

---
+++ evaluate.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 02:29:48 2018
+
+@author: matsumi
+"""
+import make_data_perm as mdp
+import load_mnist
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+import time
+import copy
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainer.optimizers
+from chainer import cuda
+from chainer import Variable, Chain, optimizers
+from chainer.cuda import cupy
+import MNIST_convnet as M
+from sklearn.metrics import pairwise_distances
+
+def softs(num_train_small_data,rank_labels,T_data,accuracy_history):
+    cheak_True_or_False = []
+    for i in range(num_train_small_data):
+        soft_top = T_data[i]==rank_labels[i]
+        cheak_True_or_False.append(np.any(soft_top))
+    average_soft_top_2_accuracy = (np.count_nonzero(cheak_True_or_False)/num_train_small_data)*100 
+    print("average_soft_top_2_accuracy:", average_soft_top_2_accuracy)
+    accuracy_history.append(average_soft_top_2_accuracy)
+    return accuracy_history
+    
